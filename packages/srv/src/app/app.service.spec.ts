918568f0aebc05cd4df5186a078008a88c3191b6
shaltaev <gleb.shaltaev@gmail.com>
2023-07-23 18:37:09 +0300

    clean up

--- packages/srv/src/app/app.service.spec.ts
+++ packages/srv/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
 	})
 
 	describe('getData', () => {
-		it('should return "Hello API"', () => {
-			expect(service.getData()).toEqual({ message: 'Hello API' })
+		it('should return "message"', () => {
+			expect(service.getData()).toEqual({ message: 'message' })
 		})
 	})
 })
---
+++ packages/utils/src/lib/utils.spec.ts
@@ -0,0 +1,7 @@
+import {utils} from './utils'
+
+describe('utils', () => {
+	it('should return "utils"', () => {
+		expect(utils()).toStrictEqual('utils')
+	})
+})
